996e4becfe74418bbc2930a2ec9006abae88882e
samadzadehartin-svg <samadzadehartin@gmail.com>
2026-07-11 10:54:55 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='deliza-v1';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
